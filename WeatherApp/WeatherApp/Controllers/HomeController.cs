b5bb53b509208056d7af78531150a0fb31f029f2
PKarunarathna <Prabodakarunarathna@gmail.com>
2020-11-07 14:27:03 +0530

    Add project

---
+++ WeatherApp/WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("WeatherOrNot");
+        }
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherIP()
+        {
+            return View();
+        }
+        public JsonResult GetWeather()
+        {
+            Weather wthr = new Weather();
+            return Json(wthr.getWeatherForecast(),JsonRequestBehavior.AllowGet);
+        }
+
+        
+    }
+}
---
+++ WeatherApp/WeatherApp/Models/Weather.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        string cityname;
+      public Object getWeatherForecast()
+      {
+      
+      string url = "http://api.openweathermap.org/data/2.5/weather?q="+cityname+"&APPID=fad7de74a958f53fa6b85a3632c426af&units=imperial";
+      var client = new WebClient();
+      var content = client.DownloadString(url);
+      var serializer = new JavaScriptSerializer();
+      var jsonContent = serializer.Deserialize<Object>(content);
+      return jsonContent;
+      }
+
+      public static string GetIPAddress()
+      {
+          string ipAddress = HttpContext.Current.Request.UserHostAddress;
+          return ipAddress;
+      }
+
+
+
+    }
+}
