2c76c2e4c3c955b35e469c8d1e136d4eeedb29ba
PKarunarathna <Prabodakarunarathna@gmail.com>
2020-11-07 19:52:46 +0530

    Final

--- WeatherApp/WeatherApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeatherApp.MvcApplication" Language="C#" %>
---
+++ WeatherWeb/WeatherWeb/Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace WeatherWeb.Models
+{
+    class Weather
+    {
+        public Object getWeatherForecast()
+        {
+            string appid = "0973222e61da15cc2474a5df3d890107";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Kandy&APPID="+ appid;
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
